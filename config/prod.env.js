4ca710c51bf3da67474fd87623ae78de71b20128
wangning <ningsy1017@163.com>
2018-09-06 19:24:30 +0800

    登录跳转错误

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@
 const baseURL = require('./global');
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST:'"http://192.168.1.15:8080/zqsign-web-wesign/"',
+  API_HOST:'"http://192.168.1.15:8080/zqsign-web-wesign/restapi/wesign/"',
   VERSION:''+new Date().getTime()+''
 }
--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -191,22 +191,14 @@ export default {
                                     });
                                 }
                             } else {
-                              this.$message({
-                                showClose: true,
-                                duration: 1000,
-                                message: "登录成功",
-                                type: "success"
-                              });
-                              cookie.set("tenant", res.data.dataList); // 存入cookie 所需信息
-                              this.$store.dispatch("tabIndex", { tabIndex: 0 }); //导航高亮
-                              this.$router.push("/Home");
+                               sessionStorage.setItem("companyList",JSON.stringify(response.data.dataList)); //角色列表
+                                this.$router.push("/Role");
                             }
                           }).catch(error => {
-                          });
 
+                          });
                     } else {
-                        sessionStorage.setItem("companyList",JSON.stringify(response.data.dataList)); //角色列表
-                        this.$router.push("/Role");
+                        
                     }
                 });
             } else {
