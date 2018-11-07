da337eb8dfbc05869c3bbecb421372e4c450e534
xialin <xialin@51signing.com>
2018-11-07 17:18:09 +0800

    样式冲突

--- src/components/Mycontract/Mycontracts.vue
+++ src/components/Mycontract/Mycontracts.vue
@@ -1,37 +1,37 @@
 <template>
-	<div class="Mycontracts">
-		<div class="main">
-			<div class="common-top">
-				<div class="common-top-tab">
-					<div class="btn-active" @click="EnterPer">企业对个人</div>
-					<div class="btn-default" style="margin-left: -5px;" @click="EnterEnter">企业对企业</div>
-				</div>
-			</div>
-			<div class='contract-type'>
-				<el-tabs v-model="activeName" tab-position="40px">
-					<el-tab-pane label="全部文件" name="first">
-						<total-contract></total-contract>
-					</el-tab-pane>
-					<el-tab-pane label="待我签署" name="second">
-						<inquiry-wait-me></inquiry-wait-me>
-					</el-tab-pane>
-						<el-tab-pane label="待他人签署" name="third">
-					<inquiry-wait-others></inquiry-wait-others>
-						</el-tab-pane>
-					<el-tab-pane label="已生效" name="fourth">
-						<inquiry-into-force></inquiry-into-force>
-					</el-tab-pane>
-					<el-tab-pane label="已截止" name="five">
-						<inquiry-expired></inquiry-expired>
-					</el-tab-pane>
-				</el-tabs>
-			</div>
-		</div>
-   </div>
+  <div class="Mycontracts">
+    <div class="main">
+      <div class="common-top">
+        <div class="common-top-tab">
+          <div class="btn-active" @click="EnterPer">企业对个人</div>
+          <div class="btn-default" style="margin-left: -5px;" @click="EnterEnter">企业对企业</div>
+        </div>
+      </div>
+      <div class='contract-type'>
+        <el-tabs v-model="activeName" tab-position="40px">
+          <el-tab-pane label="全部文件" name="first">
+            <total-contract></total-contract>
+          </el-tab-pane>
+          <el-tab-pane label="待我签署" name="second">
+            <inquiry-wait-me></inquiry-wait-me>
+          </el-tab-pane>
+          <el-tab-pane label="待他人签署" name="third">
+            <inquiry-wait-others></inquiry-wait-others>
+          </el-tab-pane>
+          <el-tab-pane label="已生效" name="fourth">
+            <inquiry-into-force></inquiry-into-force>
+          </el-tab-pane>
+          <el-tab-pane label="已截止" name="five">
+            <inquiry-expired></inquiry-expired>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss">
 
-@import "../../common/styles/content.scss";
+  @import "../../common/styles/content.scss";
   .el-tabs__nav-scroll{
     font-size: 16px;
     color: #333;
@@ -39,73 +39,76 @@
   #tab-first,#tab-second,#tab-third,#tab-fourth,#tab-five{
     font-size: 16px;
   }
-	.el-table th>.cell{
-		text-align: center;
-	}
-	#app{
-		overflow: hidden;
-	}
-	.el-table__row .cell{
-			overflow: hidden;
-			text-overflow: ellipsis;
-			white-space: nowrap;
-	}
-	.contract-type{
-		.el-tabs__header{
-			background: #fff;
-			margin: 0 auto;
-		}
-		.el-tabs__nav-scroll{
-			line-height: 58px; 
-			padding:0 25px;
-		}
-		.el-tabs__item{
-			height:58px;
-			line-height: 58px;
-		}
-		.el-tabs__item.is-active {
-			color: #4091fb;
-			border-bottom: 2px solid #4091fb;
-		}
+  .el-table th>.cell{
+    text-align: center;
+  }
+  #app{
+    overflow: hidden;
+  }
+  .el-table__row .cell{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .Mycontracts{
+  .contract-type{
+  .el-tabs__header{
+    background: #fff;
+    margin: 0 auto;
+  }
+  .el-tabs__nav-scroll{
+    line-height: 58px;
+    padding:0 25px;
+  }
+  .el-tabs__item{
+    height:58px;
+    line-height: 58px;
+  }
+  .el-tabs__item.is-active {
+    color: #4091fb;
+    border-bottom: 2px solid #4091fb;
+  }
 
-	}
+  }
+  }
+
 
 </style>
 
 <script>
-import TotalContract from './TotalContract'
-import InquiryWaitMe from './InquiryWaitMe'
-import InquiryWaitOthers from './InquiryWaitOthers'
-import InquiryIntoForce from './InquiryIntoForce'
-import InquiryExpired from './InquiryExpired'
-export default {
-  name: 'Mycontracts',
-  components: { TotalContract,InquiryWaitMe,InquiryWaitOthers,InquiryIntoForce,InquiryExpired },
-  data() {
-    return {
+  import TotalContract from './TotalContract'
+  import InquiryWaitMe from './InquiryWaitMe'
+  import InquiryWaitOthers from './InquiryWaitOthers'
+  import InquiryIntoForce from './InquiryIntoForce'
+  import InquiryExpired from './InquiryExpired'
+  export default {
+    name: 'Mycontracts',
+    components: { TotalContract,InquiryWaitMe,InquiryWaitOthers,InquiryIntoForce,InquiryExpired },
+    data() {
+      return {
         // activeName:sessionStorage.getItem('second')
-      activeName:'first',
-    //   isActive:true
-    }
-  },
-  methods:{
-    EnterPer:function () {
-      this.$router.push("/Mycontract")
+        activeName:'first',
+        //   isActive:true
+      }
     },
-    EnterEnter:function () {
-      this.$router.push("/CompanyContract")
+    methods:{
+      EnterPer:function () {
+        this.$router.push("/Mycontract")
+      },
+      EnterEnter:function () {
+        this.$router.push("/CompanyContract")
+      }
     }
-  }
 
-}
+  }
 </script>
 <style lange='css' scoped>
-@import '../../styles/Multiparty/Multiparties.scss';
-.el-button--primary:focus{
-  background: #eee;
-  border-color: #95989d;
-  color: #333;
-}
+  @import '../../styles/Multiparty/Multiparties.scss';
+  .el-button--primary:focus{
+    background: #eee;
+    border-color: #95989d;
+    color: #333;
+  }
 </style>
 
 
