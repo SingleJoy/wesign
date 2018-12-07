5351ff7d1db582243150de8da0744e0979c40741
SingleJoy <2511881919@qq.com>
2018-12-07 11:36:09 +0800

    图片路径优化

--- src/components/CompanyExb/CompanyExbm.vue
+++ src/components/CompanyExb/CompanyExbm.vue
@@ -133,7 +133,7 @@
     padding-right: 50px;
   }
   .back-home{
-    background: url("../../../static/images/ContractInfo/back-home.png") no-repeat 10px 10px;
+    background: url("/static/images/ContractInfo/back-home.png") no-repeat 10px 10px;
     width: 60px;height: 30px;padding-left:35px;color: #333;line-height: 45px;vertical-align: middle;
   }
   .el-table--scrollable-x .el-table__body-wrapper{
