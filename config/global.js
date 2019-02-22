9cf5331b099df474885e6fef5a847f3a9877946f
SingleJoy <2511881919@qq.com>
2019-02-22 11:33:55 +0800

    1.7.3

--- config/global.js
+++ config/global.js
@@ -3,7 +3,7 @@
 module.exports = {
 
   BASE_URL:'http://192.168.2.156:8080/zqsign-web-wesign',
-//   BASE_URL:'http://testwesign.zqsign.com'
+   //  BASE_URL:'http://testwesign.zqsign.com'
   // BASE_URL:'https://www.zqsign.com'
 
 }
