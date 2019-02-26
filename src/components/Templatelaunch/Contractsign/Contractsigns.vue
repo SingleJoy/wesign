49bacbc985579016047d6ef6f4677be276a3325c
Abriams <Abriams@163.com>
2019-02-26 15:06:14 +0800

    修改

--- src/components/Templatelaunch/Contractsign/Contractsigns.vue
+++ src/components/Templatelaunch/Contractsign/Contractsigns.vue
@@ -68,7 +68,7 @@
                 <div class="sign_element">
                     <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="140px" class="demo-ruleForm">
                         <el-form-item label="请输入签署密码：" prop="password">
-                            <el-input type="password" placeholder="请输入签署密码" v-model="ruleForm.password" autocomplete="off"></el-input>
+                            <el-input type="password" placeholder="请输入签署密码" maxlength="16" v-model="ruleForm.password" autocomplete="off"></el-input>
                         </el-form-item>
                     </el-form>
                 </div>
