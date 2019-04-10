a65f8c482964329f6949ad2d6665ba3376cada86
wangning <wangning@51signing.com>
2019-04-10 16:03:35 +0800

    微签实名认证完成更新session

--- src/components/EnterpriseCertificate/EnterpriseRegisterSucc/EnterpriseRegisterSuccs.vue
+++ src/components/EnterpriseCertificate/EnterpriseRegisterSucc/EnterpriseRegisterSuccs.vue
@@ -168,8 +168,8 @@
         };
         let urlParam = sessionStorage.getItem('interfaceCode')
          homePage(param,urlParam).then(res => {
-          cookie.set("tenant", res.data.dataList);  //更新cookie
-           sessionStorage.setItem("auditSteps",res.data.dataList.auditSteps)
+            cookie.set("tenant", res.data.dataList);  //更新cookie
+            sessionStorage.setItem('enterpriseName',res.data.dataList[1].companyName)
         }).catch(error=>{
 
         })
