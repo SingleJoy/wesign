7756b003b3bf885ca9f1e8e0d5b9807e3aafd24b
wangning <wangning@51signing.com>
2019-05-27 10:56:25 +0800

    文件夹

---
+++ src/components/BatchImport/ImportData/ImportData.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='BuyProduct'>
+        <!-- <BuyProducts></BuyProducts> -->
+        <Bottom></Bottom>
+    </div>
+</template>
+<script>
+// import MuTop from '../../common/components/MuTop'
+import ImportData from './ImportData'
+// import Bottom from '../../common/components/Bottom'
+export default {
+  name: 'ImportData',
+  components: {
+    ImportData,
+    // Bottom
+  }
+}
+</script>
