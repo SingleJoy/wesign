2d1c106568dbe3ed5ac82de2d6f4e351c0b5ccc5
SingleJoy <2511881919@qq.com>
2019-05-27 13:54:47 +0800

    1.9 订单

---
+++ src/components/BatchImport/OrderList/OrderList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="OrderList">
+    <Top></Top>
+    <OrderLists></OrderLists>
+    <Bottom></Bottom>
+  </div>
+</template>
+<script>
+import OrderLists from './OrderLists';
+import Bottom from '@/common/components/Bottom';
+import Top from "../../../common/components/DemoTop";
+  export default {
+    name: 'OrderList',
+    components:{
+      OrderLists,
+      Top,
+      Bottom
+    },
+    data () {
+      return {
+
+      }
+    },
+
+    created() {
+
+    }
+  }
+</script>
+<style lang="scss" scoped>
+
+  .OrderLists{
+
+  }
+</style>
+
